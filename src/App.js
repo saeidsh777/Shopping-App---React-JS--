503f364c3616985baba6fef9dcba21c4cc4c29e6
SAEID SHOJAEI <saeidshojaei71@gmail.com>
2023-07-16 13:40:59 +0330

    create shopping app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Shop from "./Components/Shop/Shop";
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Shop />
+      </div>
+    );
+  }
+}
